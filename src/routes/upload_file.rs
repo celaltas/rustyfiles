1b1b870be16d44cce0a1e294dfd420060da1361e
celaltas <60509470+celaltas@users.noreply.github.com>
2024-04-11 14:07:19 +0300

    [REF] Upload refactored.

--- src/routes/upload_file.rs
+++ src/routes/upload_file.rs
@@ -1,12 +1,13 @@
 use actix_multipart::Multipart;
 use actix_web::{
+    error::ErrorInternalServerError,
     post,
     web::{self, Redirect},
-    Error, HttpResponse,
+    Error,
 };
 use chrono::{DateTime, Local};
 use futures_util::StreamExt as _;
-use std::{io::Write, path::PathBuf};
+use std::{fs, io::Write, path::PathBuf};
 use surrealdb::{engine::remote::ws::Client, Surreal};
 
 use crate::domain::{DBFileRecord, FileRecord};
@@ -20,39 +21,55 @@ pub async fn upload_file(
     while let Some(item) = payload.next().await {
         let mut field = item?;
         let content_type = field.content_disposition();
-        let file_name = content_type.get_filename().unwrap();
-        let mut path = std::env::current_dir().unwrap();
+        let file_name = content_type.get_filename().ok_or(ErrorInternalServerError(
+            "Filename can not be read from requets",
+        ))?;
+        let mut path = std::env::current_dir()?;
         path.push(storage_path.as_str());
         path.push(file_name);
 
-        if let Ok(mut file) = std::fs::File::create(&path) {
+        if let Ok(mut file) = fs::File::create(&path) {
             while let Some(chunk) = field.next().await {
-                file.write_all(&chunk?).unwrap();
+                file.write_all(&chunk?)?;
             }
-            create_file_record(&path, &db).await.unwrap();
+            create_file_record(&path, &db).await?;
         }
     }
 
     Ok(Redirect::to("/").see_other())
 }
 
-async fn create_file_record(
-    path: &PathBuf,
-    db: &web::Data<Surreal<Client>>,
-) -> surrealdb::Result<()> {
-    let filename = path.file_name().unwrap().to_string_lossy().into_owned();
-    let size = path.metadata().unwrap().len();
+async fn create_file_record(path: &PathBuf, db: &web::Data<Surreal<Client>>) -> Result<(), Error> {
+    let filename = path
+        .file_name()
+        .ok_or(ErrorInternalServerError(
+            "Filename can not be get from path",
+        ))?
+        .to_string_lossy()
+        .into_owned();
+    let size = path
+        .metadata()
+        .map_err(|err| ErrorInternalServerError(err.to_string()))?
+        .len();
     let mime = mime_guess::from_path(&path)
         .first_or_octet_stream()
         .to_string();
-    let current_local: DateTime<Local> = Local::now();
+    let current_local = Local::now();
     let record = FileRecord {
         filename: filename.clone(),
+        path: path
+            .to_str()
+            .ok_or(ErrorInternalServerError("File path not found"))?
+            .to_string(),
         size: size,
         mime_type: mime,
         created_at: current_local,
     };
-    let _created: Vec<DBFileRecord> = db.create("files").content(record).await?;
+    let _created: Vec<DBFileRecord> = db
+        .create("files")
+        .content(record)
+        .await
+        .map_err(|err| ErrorInternalServerError(err.to_string()))?;
 
     Ok(())
 }
